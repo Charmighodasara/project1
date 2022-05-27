1526957a93f7b7dd863dd72fa8f2e18dc520e124
Charmighodasara <charmighodasara9@gmail.com>
2022-05-27 19:10:05 +0530

    changes27-05

---
+++ src/components/Demo.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Demo extends Component {
+    render() {
+        return (
+            <div>
+                <h2>hellow , i am charmi</h2>
+            </div>
+        );
+    }
+}
+
+export default Demo;
