73dbe5057e20d6f58bd88eb192e2243845c9decc
Charmighodasara <charmighodasara9@gmail.com>
2022-05-30 15:29:49 +0530

    changes3

--- src/App.js
+++ src/App.js
@@ -3,14 +3,26 @@ import './App.css';
 import Demo from './components/Demo';
 
 function App() {
-  const person = {
-    name: "Amit",
-    age: 25
-}
+  // task-1 
+  // const person = {
+  //   name: "Amit",
+  //   age: 25
+  // }
+  // console.log(person.name, person.age);
 
-console.log(person.name , person.age);
-  
+  // task-2 
+  const person = [
+    {
+      name: "Amit",
+      age: 25,
+    },
+    {
+      name: "Piyush",
+      age: 40,
+    },
+  ];
 
+    person.map((p) => console.log(p.name, p.age));
 }
 
 export default App;
