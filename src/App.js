ad51d83d25ba632ac604ed5823bb8ae98ab54250
Charmighodasara <charmighodasara9@gmail.com>
2022-05-30 15:40:52 +0530

    task-3 changes

--- src/App.js
+++ src/App.js
@@ -11,18 +11,39 @@ function App() {
   // console.log(person.name, person.age);
 
   // task-2 
-  const person = [
-    {
-      name: "Amit",
-      age: 25,
-    },
-    {
-      name: "Piyush",
-      age: 40,
-    },
-  ];
+  // const person = [
+  //   {
+  //     name: "Amit",
+  //     age: 25,
+  //   },
+  //   {
+  //     name: "Piyush",
+  //     age: 40,
+  //   },
+  // ];
+  // person.map((p) => console.log(p.name, p.age));
 
-    person.map((p) => console.log(p.name, p.age));
+  // task-3 
+  // const person = {
+  //   name: "Amit",
+  //   age: 25,
+  //   course: [
+  //     "C",
+  //     "HTML"
+  //   ]
+  // }
+
+  // for (let k in person) {
+  //   if (k === 'course') {
+  //     person[k].map((p) => console.log(p));
+  //   } else {
+  //     console.log(person[k]);
+  //   }
+  // }
+
+  // task-4 
+  
+
 }
 
 export default App;
