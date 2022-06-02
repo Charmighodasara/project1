298dab64d6fb5cf826272659126603b5f9f3a229
Charmighodasara <charmighodasara9@gmail.com>
2022-06-02 15:16:29 +0530

    changes3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 
 import './App.css';
 import Demo from './components/Demo';
+import Country from './containers/Country';
 
 function App() {
   return (
     <div>
-   
+      <Country />
     </div>
   );
 }
--- src/containers/Country.js
+++ src/containers/Country.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
 
 class Country extends Component {
+
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            countryName : 'India',
+        }
+    }
+    
+    changeCountry = () =>{
+        this.setState({
+            countryName : 'us',
+        })
+    }
+   
     render() {
         return (
             <div>
-                <p></p>
+                <p>{this.state.countryName}</p>
+                <button onClick={this.changeCountry}>change country</button>              
             </div>
         );
     }
