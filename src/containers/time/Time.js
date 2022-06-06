b4395344d5b3aa93ee90ddbe2e67c009ca345aa9
Charmighodasara <charmighodasara9@gmail.com>
2022-06-06 15:41:35 +0530

    chanes4

--- src/containers/time/Time.js
+++ src/containers/time/Time.js
@@ -1,10 +1,42 @@
 import React, { Component } from 'react';
 
 class Time extends Component {
+    //1.
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            time: new Date()
+        }
+    }
+
+    tick = () => {
+        this.setState({
+            time: new Date()
+        })
+    }
+
+    //3.
+    componentDidMount = () => {
+        this.setI = setInterval(() => this.tick(), 1000);
+    }
+
+    //4.
+    componentDidUpdate = (prevProps, prevState) => {
+        if (prevState !== this.setI) {
+            console.log("componentDidUpdate");
+        }
+    }
+
+    //5.
+    componentWillUnmount = () => {
+        clearInterval(this.setI);
+    }
+    //2.
     render() {
         return (
             <div>
-                    
+                <p>{this.state.time.toLocaleTimeString()}</p>
             </div>
         );
     }
