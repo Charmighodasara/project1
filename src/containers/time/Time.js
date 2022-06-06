e35d100ff26012c889c7983eb46f087f7ad4cc7d
Charmighodasara <charmighodasara9@gmail.com>
2022-06-06 15:09:14 +0530

    changes

---
+++ src/containers/time/Time.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Time extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Time;
