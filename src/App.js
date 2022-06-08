0c459eda38e35fda00f4338fcef5ad33997e9274
Charmighodasara <charmighodasara9@gmail.com>
2022-06-08 11:07:22 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
 
 import './App.css';
-import Demo from './components/Demo';
-import City from './containers/City';
-import CityFun from './containers/CityFun';
+
 import Counter from './containers/Counter/Counter';
 import CounterFun from './containers/Counter/CounterFun';
-import Country from './containers/Country';
-import CountryFun from './containers/CountryFun';
-import Time from './containers/time/Time';
-import TimeFunctin from './containers/time/TimeFunctin';
 
 function App() {
   return (
