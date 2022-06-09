cda022c55813d5bf56b35ef1b4aadaa41d28ffa3
Charmighodasara <charmighodasara9@gmail.com>
2022-06-09 11:54:24 +0530

    changes4

--- src/App.js
+++ src/App.js
@@ -8,25 +8,25 @@ import Home from './containers/Home';
 const HomewithLoader = Loader(Home)
 
 
-function App() {  
+function App() {
   const [loader, setLoader] = useState(false)
-  const [data , setData] = useState([])
- 
+  const [data, setData] = useState([])
+
 
   const orgData = [
-    {id:101 , name: 'charmi'},
-    {id:102 , name: 'ghodasara'}
+    { id: 101, name: 'charmi' },
+    { id: 102, name: 'ghodasara' }
   ]
- 
-  useEffect(()=>{
+
+  useEffect(() => {
     setLoader(true);
-    setTimeout(()=>{setLoader(false); setData(orgData)},2000)
+    setTimeout(() => { setLoader(false); setData(orgData) }, 2000)
 
   }, [])
 
   return (
     <div>
-      <HomewithLoader isLoading={loader} data={data}/>
+      <HomewithLoader isLoading={loader} data={data} />
     </div>
   );
 }
